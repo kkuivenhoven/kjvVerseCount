99243990aaa5350ce7258d0a4fb5583a0d74b5e8
kkuivenhoven <kkuivenhoven@mail.csuchico.edu>
2020-05-07 22:58:34 -0700

    init function set up

--- lib/verse_count.rb
+++ lib/verse_count.rb
@@ -4,12 +4,31 @@ require 'open-uri'
 class KjvVerseCount
 	@wholeBible = Hash.new
 
+=begin
 	def initialize(name, chapter, verseTotal)
 		@name = name
 		@chapter = chapter
 		@verseTotal = verseTotal
 	end
+=end
 
 	def self.init
-		CSV.new(open("https://raw.githubusercontent.com/kkuivenhoven/kjvVerseCount/master/lib/kjvVerseCount.csv")
+		puts "inside init"
+		@wholeBible = Hash.new
+		CSV.new(open("https://raw.githubusercontent.com/kkuivenhoven/kjvVerseCount/master/lib/kjvVerseCount.csv")).each do |bookChapterLine|
+			if @wholeBible.has_key?(bookChapterLine[0])
+				tmpHash = { bookChapterLine[1] => bookChapterLine[2] }
+				@wholeBible[bookChapterLine[0]] << tmpHash
+			else
+				@wholeBible[bookChapterLine[0]] = Hash.new
+				@wholeBible[bookChapterLine[0]] = []
+				tmpHash = { bookChapterLine[1] => bookChapterLine[2] } 
+				@wholeBible[bookChapterLine[0]] << tmpHash
+			end
+		end
+	end
 
+end
+
+KjvVerseCount.new
+KjvVerseCount.init
